e53bb5b791e25896893262ff549b75369d4ea097
fajardo <fajardo90@correo.ugr.es>
2019-09-22 17:11:44 +0200

    tutorial qwt

---
+++ src/main/java/me/client/App.java
@@ -0,0 +1,21 @@
+package me.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.intendia.rxgwt2.elemento.RxElemento;
+import elemental2.dom.HTMLElement;
+import org.jboss.gwt.elemento.core.Elements;
+
+import static elemental2.dom.DomGlobal.document;
+import static org.jboss.gwt.elemento.core.EventType.mousemove;
+
+public class App implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+
+        HTMLElement el = Elements.span().style("font-weight: bold;").get();
+        Elements.body().add("mouse position: ").add(el);
+        RxElemento.fromEvent(document, mousemove)
+                .subscribe(ev -> el.textContent = ev.clientX + ", " + ev.clientY);
+    }
+}
